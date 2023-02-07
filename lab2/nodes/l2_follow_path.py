6d147588376b278fc8ddd9dfe1ab463cfa395a02
Cameron <cameron@vapium.com>
2023-02-06 19:41:32 -0500

    Task 8
    Included trajectory rollout algorithm in follow_path.py - still needs to be debugged and tested.
    
    Path options are generated using trajectory rollout function from l2_planning.py, which also checks for collisions. Then options are scored and the one with the minimal cost (calculated 4 ways, weights can be tweaked) is selected for control input.

--- lab2/nodes/l2_follow_path.py
+++ lab2/nodes/l2_follow_path.py
@@ -13,6 +13,12 @@ from geometry_msgs.msg import TransformStamped, Twist, PoseStamped
 from nav_msgs.msg import Path, Odometry, OccupancyGrid
 from visualization_msgs.msg import Marker
 
+from l2_planning import PathPlanner
+from l2_planning import Node
+# from nodes.l2_planning import PathPlanner
+# from nodes.l2_planning import Node
+# [CAM] Not sure which import statement is proper
+
 # ros and se2 conversion utils
 import utils
 
@@ -36,9 +42,13 @@ TEMP_HARDCODE_PATH = [[2, -.5, 0], [2.4, -1, -np.pi/2], [2.45, -3.5, -np.pi/2], 
 
 
 class PathFollower():
-    def __init__(self):
+    def __init__(self, planner):
         # time full path
         self.path_follow_start_time = rospy.Time.now()
+        
+        # [CAM] load planner so we may use occupancy map & planner functions
+        self.planner = planner
+        self.previous_final_cost = np.zeros(self.num_opts)
 
         # use tf2 buffer to access transforms between existing frames in tf tree
         self.tf_buffer = tf2_ros.Buffer()
@@ -122,31 +132,61 @@ class PathFollower():
             self.check_and_update_goal()
 
             # start trajectory rollout algorithm
-            local_paths = np.zeros([self.horizon_timesteps + 1, self.num_opts, 3])
-            local_paths[0] = np.atleast_2d(self.pose_in_map_np).repeat(self.num_opts, axis=0)
+            # local_paths = np.zeros([self.horizon_timesteps + 1, self.num_opts, 3])
+            # local_paths[0] = np.atleast_2d(self.pose_in_map_np).repeat(self.num_opts, axis=0)
 
-            print("TO DO: Propogate the trajectory forward, storing the resulting points in local_paths!")
-            for t in range(1, self.horizon_timesteps + 1):
-                # propogate trajectory forward, assuming perfect control of velocity and no dynamic effects
-                pass
+            # [CAM]
+            local_paths = self.planner.trajectory_rollout(self.all_opts_scaled[:, 0], self.all_opts_scaled[:, 1], self.pose_in_map_np[:, None], fix_col=True)
+            local_paths = np.transpose(local_paths, (1, 0, 2))
+            # local_paths is of shape (num_substeps, N, 3)
+            # Using fix_col, we stop rolling out after one of the trajectories collides.
+            # Thus, collisions are already checked.
+
 
             # check all trajectory points for collisions
             # first find the closest collision point in the map to each local path point
-            local_paths_pixels = (self.map_origin[:2] + local_paths[:, :, :2]) / self.map_resolution
+            # local_paths_pixels = (self.map_origin[:2] + local_paths[:, :, :2]) / self.map_resolution
+            # valid_opts = range(self.num_opts)
+            # local_paths_lowest_collision_dist = np.ones(self.num_opts) * 50
+
+            # print("TO DO: Check the points in local_path_pixels for collisions")
+            # for opt in range(local_paths_pixels.shape[1]):
+            #     for timestep in range(local_paths_pixels.shape[0]):
+            #         pass
+
+            # [CAM] - Collision checking is done inside trajectory rollout, don't need to do here
             valid_opts = range(self.num_opts)
-            local_paths_lowest_collision_dist = np.ones(self.num_opts) * 50
 
-            print("TO DO: Check the points in local_path_pixels for collisions")
-            for opt in range(local_paths_pixels.shape[1]):
-                for timestep in range(local_paths_pixels.shape[0]):
-                    pass
-
-            # remove trajectories that were deemed to have collisions
-            print("TO DO: Remove trajectories with collisions!")
+            # [CAM] 4 ways to calculate path score are shown below. Final cost will be a linear combination of these
 
             # calculate final cost and choose best option
             print("TO DO: Calculate the final cost and choose the best control option!")
             final_cost = np.zeros(self.num_opts)
+
+            # Score based on dist to cur_goal
+            cost_dist2_to_goal = np.sum(np.square(local_paths[-1, :, 0:2] - self.cur_goal[None, :]), axis=1)
+            weight_dg = -0.2
+
+            # Score based on final dist to nearest obstacle
+            obstaclePoints = self.planner.cell_to_point(np.where(self.planner.occupancy_map == 0)) # (N_obs, 2)
+            cost_dist2_to_obs1 = local_paths[-1, :, None, 0:2] - obstaclePoints[None, :, :]
+            # broadcasting:      (N, 1, 2)                     - (1, N_obs, 2)
+            cost_dist2_to_obs1 = np.min(np.sum(np.square(cost_dist2_to_obs1), axis=2), axis=1)
+            weight_do1 = 0.2
+
+            # Score based on dist to nearest obstacle at any point in trajectory (not sure if can be computed in time)
+            obstaclePoints = self.planner.cell_to_point(np.where(self.planner.occupancy_map == 0)) # (N_obs, 2)
+            cost_dist2_to_obs2 = local_paths[:, :, None, 0:2] - obstaclePoints[None, None, :, :]
+            # broadcasting:      (num_substeps, N, 1, 2)      - (1, 1, N_obs, 2)
+            cost_dist2_to_obs2 = np.min(np.sum(np.square(cost_dist2_to_obs2), axis=3), axis=(0, 2))
+            weight_do2 = 0.2
+
+            # Score based on similarity to previous
+            cost_previous = self.previous_final_cost
+            weight_cp = 0.1
+
+            final_cost = weight_dg * cost_dist2_to_goal + weight_do1 * cost_dist2_to_obs1 + weight_cp * cost_previous
+
             if final_cost.size == 0:  # hardcoded recovery if all options have collision
                 control = [-.1, 0]
             else:
@@ -197,6 +237,16 @@ class PathFollower():
         self.cmd_pub.publish(Twist())
         rospy.loginfo("Published zero vel on shutdown.")
 
+def main():
+    map_filename = "willowgarageworld_05res.png"
+    map_setings_filename = "willowgarageworld_05res.yaml"
+    goal_point = np.array([[10], [10]]) #m
+    stopping_dist = 0.5
+    planner = PathPlanner(map_filename, map_setings_filename, goal_point, stopping_dist)
+
+    path_follower = PathFollower(planner)
+
+    path_follower.follow_path()
 
 if __name__ == '__main__':
     try:
